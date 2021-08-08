aaa7543a2a93ef28a5f4d10c3ef4214d696ecf6a
Douglas Viana Coutinho <douglasvcoutinho@hotmail.com>
2021-08-07 22:33:01 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,23 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+
+    public abstract String getFirstPartResult(Customer aCustomer);
+
+    public abstract String getMiddlePartResult(Rental aRental);
+
+    public abstract String getFinalPartResult(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getFirstPartResult(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += getMiddlePartResult(each);
+        }
+        // add footer lines
+        result += getFinalPartResult(aCustomer);
+        return result;
+    }
 }
